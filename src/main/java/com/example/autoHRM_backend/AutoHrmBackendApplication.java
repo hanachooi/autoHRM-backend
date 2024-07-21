b6dfe6ae1709858ca29fc784146035944144fb35
hanachooi <jjina.js@gmail.com>
2024-07-21 22:58:12 +0900

    Initial commit

---
+++ src/main/java/com/example/autoHRM_backend/AutoHrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.autoHRM_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoHrmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoHrmBackendApplication.class, args);
+	}
+
+}
